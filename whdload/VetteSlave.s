65c605e774423c72b29df2e114bc5b4517baddad
Vesa Halttunen <vesuri@jormas.com>
2026-09-25 10:40:27 +0200

    Label the WHDLoad display option High resolution mode

--- whdload/VetteSlave.s
+++ whdload/VetteSlave.s
@@ -36,7 +36,7 @@ slv_copy dc.b "1991 Sphere, Inc.",0
 slv_info dc.b "Amiga port by Vesuri",10
         dc.b "Version 0.91 (25.09.2026)",10
         dc.b "F10 quits",0
-slv_config dc.b "C1:B:HIRES (512x320 interlaced);",0
+slv_config dc.b "C1:B:High resolution mode;",0
         dc.b "$VER: Vette!.slave 0.91 (25.09.2026)",0
 _program dc.b "Vette!",0
 _args dc.b 10
